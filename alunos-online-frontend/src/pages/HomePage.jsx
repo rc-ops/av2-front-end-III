1e86fc022edcfd07c1eef1d59365ebab12c7354b
rc-ops <rodrigocarvalhocontato@protonmail.com>
2023-05-28 15:41:13 -0300

    Adds home page

--- alunos-online-frontend/src/pages/HomePage.jsx
+++ alunos-online-frontend/src/pages/HomePage.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import logo from '../assets/logo-uniesp.jpg';
+import pencil_square from '../assets/pencil-square.svg';
+import viewer from '../assets/viewer.ico';
+import { Card, Button } from 'react-bootstrap';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <div className="container">
+        <div className="jumbotron">
+          <div className="page-header"><img src={logo} alt="Logo UNIESP" />;</div>
+          <h2><del>Web design is my passion</del></h2>
+          <br />
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <Card>
+              <Card.Img variant="top" src={viewer}
+                style={{
+                  width: '25%',
+                  height: '25%',
+                  objectFit: 'cover'
+                }}
+              />
+              <Card.Body>
+                <Card.Title>Lista de alunos</Card.Title>
+                <Card.Text>
+                  Clique aqui para ver a relação de alunos
+                </Card.Text>
+                <Button variant="primary" href="/alunos">Alunos</Button>
+              </Card.Body>
+            </Card>
+          </div>
+
+          <div className="col-sm-6">
+            <Card>
+              <Card.Img variant="top" src={pencil_square}
+                style={{
+                  width: '25%',
+                  height: '25%',
+                  objectFit: 'cover'
+                }} />
+              <Card.Body>
+                <Card.Title>Cadastro</Card.Title>
+                <Card.Text>
+                  No link abaixo podes realizar o cadastro de um aluno
+                </Card.Text>
+                <Button variant="primary" href="/cadastro">Cadastro</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
-
-export default HomePage
+export default HomePage;
